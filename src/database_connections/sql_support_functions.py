aec61e1af9fa33bb069bd87dda8af31ca1a07ae5
James Kabbes <james.kabbes@gmail.com>
2022-06-08 21:09:19 -0500

    simplifying imports, improving running queries

--- src/database_connections/sql_support_functions.py
+++ src/database_connections/sql_support_functions.py
@@ -2,23 +2,18 @@ import dir_ops as do
 import py_starter as ps
 import os
 import database_connections.Query as Query
+import database_connections.Connections as Connections
+import importlib 
 
-script_Path = do.Path( os.path.abspath(__file__) )
-sql_Dir = script_Path.ascend() 
 
-def get_DatabaseConnection( connection_module = 'teradata', **kwargs ):
+def get_DatabaseConnection( connection_module = 'sqlite', **kwargs ):
 
     '''returns a class object from the given SQL connection module'''
 
-    # list out the options available in the SQL folder: Oracle, Redshift, SQLite, Teradata
-    module_options_Dirs = sql_Dir.list_contents_Paths( block_dirs = False, block_paths = True )
-
-    for module_options_Dir in module_options_Dirs:
-        if module_options_Dir.dirs[-1].lower() == connection_module.lower():
-            module = ps.import_module_from_path( module_options_Dir.join( module_options_Dir.dirs[-1] + '_connection.py' ))
-            return module.get_DatabaseConnection( **kwargs )
-
-    return None
+    Conns_obj = importlib.__import__( 'database_connections.Connections', fromlist=[connection_module] )
+    module = getattr( Conns_obj, connection_module )
+    conn = module.get_DatabaseConnection( **kwargs )
+    return conn
 
 def list_to_string( iterable, sep = ',', quotes = "'" ):
 
@@ -29,32 +24,22 @@ def list_to_string( iterable, sep = ',', quotes = "'" ):
 
     return string
 
-def run_queries_in_folder( queries_Dir, export_Dir, engine = 'teradata', export_type = 'csv', ask_to_run = True, print_df = True, parquet_engine = 'fastparquet', conn_inst = None, **connection_module_params ):
+def run_queries_in_folder( queries_Dir, export_Dir, conn_inst, print_df: bool = True, export_type = 'csv', parquet_engine = 'fastparquet', file_endings = ['sql'] ):
 
-    sql_types = ['sql']
+    """Given a Dir of Queries, an Export Dir, and a database connection, run all queries as specified by the user"""
+
     sql_Paths = queries_Dir.list_contents_Paths( block_dirs = True )
+    
+    valid_Paths = do.Paths()
+    for sql_Path in sql_Paths:
+        if sql_Path.ending in file_endings:
+            valid_Paths._add( sql_Path )
 
-    if conn_inst == None:
-        conn_inst = get_DatabaseConnection( connection_module = engine, **connection_module_params )
-    conn_inst.init()
+    to_run_Paths = ps.get_selections_from_list( valid_Paths )
 
-    ### only select queries which have correct endings
-    for i in range(len(sql_Paths)-1,-1,-1):
-        sql_Path = sql_Paths[i]
-        if sql_Path.ending.lower() not in sql_types:
-            del sql_Paths[i]
+    for sql_Path in to_run_Paths:
 
-    sql_paths = [ i.path for i in sql_Paths ]
-
-    ### Ask the users which queries which they want to run
-    inds_to_run = list(range(len(sql_Paths)))
-    if ask_to_run:
-        inds_to_run = ps.get_user_selection_for_list_items(sql_paths, prompt = 'Select which queries to run - enter to exit', print_off = True)
-
-    ### Execute the queries
-    for i in inds_to_run:
-        sql_Path = sql_Paths[i]
-        export_Path = do.Path( export_Dir.join( sql_Path.root + '.' + export_type ) )
+        export_Path = export_Dir.join_Path( path = sql_Path.root + '.' + export_type ) 
 
         export_kwargs = {}
         if export_Path.ending == 'parquet':
@@ -67,5 +52,3 @@ def run_queries_in_folder( queries_Dir, export_Dir, engine = 'teradata', export_
         if print_df:
             sql_Query.df
 
-    # close out of the connection
-    conn_inst.exit()
